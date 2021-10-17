f48c3d825a5ea12a49b602c4bfb3f29c572463b4
alecs000 <alecs.2004fed@gmail.com>
2021-10-17 20:58:42 +0300

    noothing

---
+++ 24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 3, 4, 5, 6, 7, 4, 2, 234, 45, 56, 4, 3, 234 };
+            for (int i = 1; i < arr.Length-1; i++)
+            {
+
+                    if (arr[i - 1] % 2 == 0 && arr[i + 1] % 2 == 0 && arr[i] < arr[i + 1] && arr[i] < arr[i - 1])
+                    {
+                    Console.WriteLine(arr[i]);
+                    }
+                
+            }
+        }
+    }
+}
